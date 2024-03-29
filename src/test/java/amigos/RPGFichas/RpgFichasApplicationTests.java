330af921a5bbd628c0f51ff5ff8d8e600771cfd9
Fernando Moreira da Silva Filho <nandinho.m.silvafilho@gmail.com>
2024-03-28 23:06:55 -0300

    Addons

---
+++ src/test/java/amigos/RPGFichas/RpgFichasApplicationTests.java
@@ -0,0 +1,13 @@
+package amigos.RPGFichas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgFichasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
