72fdd907b7e837c80941d75fa953b13e9d1f4f6c
GabrielNunes <gabrielnunescamposgb@gmail.com>
2025-01-30 12:26:32 -0300

    Icone
    Testando icone antigo

--- rpgfront-end/src/App.tsx
+++ rpgfront-end/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Contatos from "./pages/Contatos";
+import Sobre from "./pages/Sobre";
+import "./styles/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+    <Router>
+      <ul className="menu">
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/contatos'>Contatos</Link>
+        </li>
+        <li>
+          <Link to='/sobre'>Sobre</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contatos" element={<Contatos />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
