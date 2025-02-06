817fe3b38922c2f00e45af88c94e7082ac6afbc9
GabrielNunes <gabrielnunescamposgb@gmail.com>
2025-02-05 22:21:57 -0300

    Versão 1

--- rpgfront-end/src/App.tsx
+++ rpgfront-end/src/App.tsx
@@ -1,31 +1,34 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Contatos from "./pages/Contatos";
-import Sobre from "./pages/Sobre";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import "./styles/style.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="parent">
-    <Router>
-      <ul className="menu">
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/contatos'>Contatos</Link>
-        </li>
-        <li>
-          <Link to='/sobre'>Sobre</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contatos" element={<Contatos />} />
-        <Route path="/sobre" element={<Sobre />} />
-      </Routes>
-    </Router>
+    // <div className="parent">
+    // <Router>
+    //   <ul className="menu">
+    //     <li>
+    //       <Link to='/'>Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to='/contatos'>Contatos</Link>
+    //     </li>
+    //     <li>
+    //       <Link to='/sobre'>Sobre</Link>
+    //     </li>
+    //   </ul>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/contatos" element={<Contatos />} />
+    //     <Route path="/sobre" element={<Sobre />} />
+    //   </Routes>
+    // </Router>
+    <div>
+      <Navbar/>
     </div>
+
+    // </div>
   );
 }
 
---
+++ rpgfront-end/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-item">Logo</div>
+      <div className="navbar-item">Home</div>
+      <div className="navbar-item">App</div>
+      <div className="navbar-item">Suporte</div>
+      <div className="navbar-item">Sobre Nois</div>
+      <div className="navbar-item">Configuração</div>
+    </div>
+  );
+}
+
+
